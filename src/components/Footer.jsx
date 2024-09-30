10bff813344037fb90ad6d53b0a4e7c63eca48f9
Emilianopombodev <emilianopombodev@gmail.com>
2024-09-29 21:24:32 -0300

    probando volver a comitear

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer style={{ marginTop: 'auto' }} className="bg-gray-200 py-3">
       <section className="container mx-auto text-center">
-        <h2 className="text-2xl font-bold mb-4">¿Interesado en nuestros productos?</h2>
+        <h2 className="text-2xl font-bold mb-4">¿Interesado en nuestros2 productos??!</h2>
         <p className="text-lg text-gray-700 mb-8">¡Contáctanos hoy mismo para obtener más información!</p>
         <a href="/contacto" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
           Contacto
